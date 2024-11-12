562a4edbf9f73615f220c571bfa65c6fcd41edc7
Jihyun3478 <leejihyun3478@gmail.com>
2024-11-13 00:32:07 +0900

    feat : 프로젝트 초기 세팅

---
+++ src/main/java/com/example/msa/user/service/MsaUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msa.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/msa/user/service/MsaUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msa.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
